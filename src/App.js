fcdfef0008bb0755177f986a70a58a373062c383
Laura <lhutt1991@gmail.com>
2020-11-11 17:20:26 +0000

    updating API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/navigation/Navigation';
 import SignIn from './components/SignIn/SignIn';
@@ -10,9 +9,9 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-const app = new Clarifai.App({
-	apiKey: '8766f36cdd4e437c910e744107444184'
-});
+// const app = new Clarifai.App({
+// 	apiKey: '8766f36cdd4e437c910e744107444184'
+// });
 
 const particlesOptions = {
 	particles: {
@@ -82,17 +81,14 @@ class App extends Component {
 
 	onButtonSubmit = () => {
 		this.setState({imageUrl: this.state.input});
-		app.models
-			.predict(Clarifai.FACE_DETECT_MODEL,
-				this.state.input)
-		// fetch('https://obscure-crag-51057.herokuapp.com/imageurl', {
-		// 	method: 'post',
-		// 	headers: {'Content-Type': 'application/json'},
-		// 	body: JSON.stringify({
-		// 		input: this.state.input
-		// 	})
-		// })
-		// .then(response => response.json())
+			fetch('https://obscure-crag-51057.herokuapp.com/imageurl', {
+				method: 'post',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify({
+					input: this.state.input
+				})
+			})
+			.then(response => response.json())
 			.then(response => {
 				if (response) {
 					fetch('https://obscure-crag-51057.herokuapp.com/image', {
