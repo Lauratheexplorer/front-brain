f7b40cf7eef035dfe4cff24df64fbfa83b802688
Laura <lhutt1991@gmail.com>
2020-12-11 11:19:52 +0000

    changing text and background of image link box

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div>
 			<p className='f3'>
-				{'This Magic Brain will detect faces in your pictures! Give it a try...'}
+				{'This Magic Brain will sometimes detect faces in your pictures! Sometimes it likes to have a lay in, so it might not want to cooperate, but you will still have a lovely picture displayed below! Why not give it a try...'}
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
